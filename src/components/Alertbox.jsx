22a5d3a5032ce1ff7b790e495c6ebc3d93754fd8
Shishir990 <shishirsaklani659@gmail.com>
2024-11-02 21:30:49 +0530

    Added changes

--- src/components/Alertbox.jsx
+++ src/components/Alertbox.jsx
@@ -2,7 +2,7 @@ import {Alert} from "react-bootstrap"
 export function Alertbox(){
     return(
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '30vh' }}>
-         <Alert style={{margin:"30px auto"}} variant="success">
+         <Alert style={{margin:"30px auto"}} variant="primary">
         <Alert.Heading>Hey,Welcome to my Weather App</Alert.Heading>
         <p>
           You can view weather reports here just by serching the name of the city
